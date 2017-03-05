52014a09583f8bf9c94d7eca37a167c42cdf180f
tarnjot singh <tarnjot singh>
2017-03-05 10:44:08 +0000

    First commit

---
+++ scrolling.js
@@ -0,0 +1,14 @@
+$(".navbar a, footer a[href='#']").on('click', function(event) {
+  if(this.hash !== "")
+  {
+    event.preventDefault();
+    var hash = this.hash;
+
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+      window.location.hash = hash;
+    });
+  }
+});
